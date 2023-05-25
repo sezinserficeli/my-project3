8731840e176b48f9e8989bc81ad8a3ec5cff378a
sezinserficeli <serficelis@gmail.com>
2023-05-25 17:46:46 +0300

    assignment3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-project3';
+  hidePassword = false;
+  buttonLogArray = [];
+  counter = 0;
+
+  toggle() {
+    this.hidePassword = !this.hidePassword;
+    this.buttonLog();
+  }
+
+  buttonLog() {
+    this.buttonLogArray.push(this.counter++);
+    console.log(this.buttonLogArray);
+  }
 }
